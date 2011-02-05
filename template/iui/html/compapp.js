5b3c0294136e1e7db4d5658a0aa4d606f8dbea9e
Wai Yau <wyau@email.unc.edu>
2011-02-04 20:04:39 -0500

    making minor changes

--- template/iui/html/compapp.js
+++ template/iui/html/compapp.js
@@ -22,10 +22,10 @@ dojo.declare('compapp',[], {
                 self.createBorderBar(self,tab,tr);
             });
        
-            var leftPhrases = ["Please give me a moment to respond.", "I don't understand.", "Can you repeat that, please?", "I need help."];
+            var leftPhrases = ["Thank you.", "How about you?", "Please give me a moment to respond.", "I don't understand.", "Can you repeat that, please?", "I need help."];
             var leftTable = dojo.create("table",{id:"sideTable",style:{"width":'12.5%',"height":'80%'},selected:"true"},dojo.body());
                 dojo.forEach(leftPhrases, function(aPhrase) {
-                    var td = dojo.create("table",{id:"sidetd1",style:{"height":"25%","width":"100%"},innerHTML:aPhrase},leftTable);
+                    var td = dojo.create("table",{id:"sidetd1",style:{"height":"16%","width":"100%"},innerHTML:aPhrase},leftTable);
                         dojo.connect(td, 'onmouseup',dojo.hitch(self,'playSound',aPhrase));
                 });
             
