6d6d9a8991f00a37617e1c9dd8187726e8a62ac5
Wai Yau <wyau@email.unc.edu>
2011-02-12 17:25:50 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -120,9 +120,8 @@
     function start() {
         var mainArea = dojo.byId("mainArea");
         var ulList = ["Step 1 -- Getting Started", "Step 2 -- Customize Your Tabs", "Step 3 -- Select Contents for Your Tabs", "Step 4 -- Summary", "Step 5 -- You're Done!"];
-        var li = dojo.create("li",null,mainArea);
         dojo.forEach(ulList, function(item) {
-            var ul = dojo.create("ul",{innerHTML:item},li);  
+            var ul = dojo.create("ul",{innerHTML:item},mainArea);  
         });
     }    
     
