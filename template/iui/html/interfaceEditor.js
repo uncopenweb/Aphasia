b3326c7b4be8d8b58aa58514adcfc83dc48cb31e
Wai Yau <wyau@email.unc.edu>
2011-02-13 14:37:04 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -135,7 +135,7 @@
                 if (i!=4) {
                     button.parentNode.style.display="none";
                     forwardButton[++i].parentNode.style.display="block";
-                    jsProgress.update({progress:"20"});
+                    jsProgress.update({progress:"1"});
                 }
             });
         });
@@ -144,7 +144,7 @@
                 if (i!=0) {
                     button.parentNode.style.display="none";
                     forwardButton[--i].parentNode.style.display="block";
-                    jsProgress.update({progress:"-20"});
+                    jsProgress.update({progress:"-1"});
                 }
             });
         });
