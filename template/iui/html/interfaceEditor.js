0bcb0b0c35c7ea9e31f4dcaf918f4f2526218e01
Wai Yau <wyau@email.unc.edu>
2011-02-13 14:59:44 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -150,9 +150,9 @@
         });
         forwardButton[0].parentNode.style.display = "block";
         dojo.forEach(ulList, function(item) {
-            var td = dojo.create("td",{id:item, innerHTML:item, style:{"font-weight":"normal"}, className:"tabs"},tabs);
+            var td = dojo.create("td",{id:item, innerHTML:item, style:{"font-style":"normal"}, className:"tabs"},tabs);
         });
-        console.log(dojo.byId(ulList[0].style.font-weight));
+        console.log(dojo.byId(ulList[0].style.font-style));
         //dojo.byId(ulList[0]).style.font-weight = "bold";
     }    
     
