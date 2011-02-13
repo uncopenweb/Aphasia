8291665ef62da84b7bb0142eec11d0b5840dc6df
Wai Yau <wyau@email.unc.edu>
2011-02-13 14:57:09 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -150,7 +150,7 @@
         });
         forwardButton[0].parentNode.style.display = "block";
         dojo.forEach(ulList, function(item) {
-            var td = dojo.create("td",{id:item, innerHTML:item, style:{}, className:"tabs"},tabs);
+            var td = dojo.create("td",{id:item, innerHTML:item, style:{"font-weight":"normal"}, className:"tabs"},tabs);
         });
         console.log(dojo.byId(ulList[0].style));
         //dojo.byId(ulList[0]).style.font-weight = "bold";
