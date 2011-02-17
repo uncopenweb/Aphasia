ca5a1c95b428dde5e1f70ccad03b231216e7c53f
Wai Yau <wyau@email.unc.edu>
2011-02-16 22:36:33 -0500

    style changes on app

--- template/iui/html/compapp.js
+++ template/iui/html/compapp.js
@@ -66,7 +66,7 @@ dojo.declare('compapp',[], {
             var td = dojo.create("td",{id:"tTd",align:"center",style:{"border-right-style":"1px solid black","width":"33.3%"}}, tr);
             var caption = dojo.create("div",{innerHTML:tab.nameTag, selected:"true", style:{"font-size":"x-large","align":"center"}},td);
             var a = dojo.create("a",{href:tab.name},td);
-            var image = dojo.create("img",{src:tab.image, width:"20%", height:"20%"}, a);
+            var image = dojo.create("img",{src:tab.image, width:"17%", height:"17%"}, a);
             dojo.connect(a, 'onmouseup', function() {
                 if (self.currentTab != tab) {
                     if (self.currentTab!=null) {
