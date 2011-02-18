08fe98c20593d0ff3915b8c213ed28a0182f244c
Wai Yau <wyau@email.unc.edu>
2011-02-17 21:46:13 -0500

    style fix

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -177,9 +177,9 @@
         var h4 = dojo.create("div",{className:"first", innerHTML:"What do you want to do?"},div);
         var form = dojo.create("form", null, div);
         var c1 = dojo.create("input",{type:"radio", value:"c", name:"1p"},form);
-        var br1 = dojo.create("br",null,div);
+        var br1 = dojo.create("br",null,form);
         var c2 = dojo.create("input",{type:"radio", value:"e", name:"1p"},form);
-        var br2 = dojo.create("br",null,div);
+        var br2 = dojo.create("br",null,form);
         var c3 = dojo.create("input",{type:"radio", value:"d", name:"1p"},form);
     }
     
