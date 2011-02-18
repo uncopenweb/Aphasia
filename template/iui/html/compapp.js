548e28b12975dd0e3a22b84827506ab7e972929f
Wai Yau <wyau@email.unc.edu>
2011-02-17 20:05:15 -0500

    style fix

--- template/iui/html/compapp.js
+++ template/iui/html/compapp.js
@@ -64,7 +64,7 @@ dojo.declare('compapp',[], {
         
         createBorderBar: function(self, tab, tr) {
             var td = dojo.create("td",{align:"center",style:{"font-size":"x-large","width":"33.3%"}}, tr);
-            var caption = dojo.create("div",{innerHTML:tab.nameTag, selected:"true", style:{"font-size":"x-large","align":"center"}},td);
+            var caption = dojo.create("div",{innerHTML:tab.nameTag, selected:"true", style:{"align":"center"}},td);
             var a = dojo.create("a",{href:tab.name},td);
             var image = dojo.create("img",{src:tab.image, width:"17%", height:"17%"}, a);
             dojo.connect(a, 'onmouseup', function() {
