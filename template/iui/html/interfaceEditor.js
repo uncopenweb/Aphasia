b2944596e6c1f44d5bb6a8bdbe0fa48ffd03ac0e
Wai Yau <wyau@email.unc.edu>
2011-02-17 21:13:05 -0500

    style fix

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -128,7 +128,7 @@
         });
         var forwardButton = [];
         var backwardButton = [];
-        var ids = ["0","1","2","3","4"];
+        var ids = this.ids = ["0","1","2","3","4"];
         for (var i=0; i<5; i++) {
             var div = dojo.create("div",{id:ids[i],className:"mainContent", style:{"display":"none"}},mainArea);
             backwardButton[i] = dojo.create("button",{innerHTML:"Back"},div);
