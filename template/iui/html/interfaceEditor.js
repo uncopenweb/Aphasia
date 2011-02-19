f7f7038c2d5f0c44ff2d3bdc36bb10b3d3814c33
Wai Yau <wyau@email.unc.edu>
2011-02-19 16:13:07 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -171,16 +171,20 @@
     function step1 (ids) {
     
         var layout = [{
-            field:"Theme",
-            name:"Theme",
-            width:"200px"
+            field: 'Title',
+            name: 'Title of Movie',
+            width: '200px'
         },
         {
-            field:"id",
-            name:"ID",
-            width:"200px"
-        }
-        ];
+            field: 'Year',
+            name: 'Year',
+            width: '50px'
+        },
+        {
+            field: 'Producer',
+            name: 'Producer',
+            width: 'auto'
+        }];
         
         var store4 = new dojox.data.CsvStore({
             url: '../../_static/jsdojox/grid/tests/support/movies.csv'
