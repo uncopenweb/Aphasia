db884313939ac0f84817eef4af1f60ea4f6c54f6
Wai Yau <wyau@email.unc.edu>
2011-02-18 22:35:56 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -204,13 +204,17 @@
             dojo.create("label",{innerHTML:"Delete an existing interface"},form);
             dojo.create("br",null,form);
             dojo.create("br",null,form);
+            dojo.create("br",null,form);
         
         var gridDiv = dojo.create("div",{className:"grid",style:{"width":"400px"},innerHTML:"Select the existing interface you want."},div);
         var grid = new dojox.grid.DataGrid({
             structure: layout
-        },dojo.create("div",{style:{"height":"30px"}},gridDiv));
+        });
         grid.startup();
         
+        dojo.create("br",null,form);
+        dojo.create("br",null,form);
+        dojo.create("br",null,form);
         var button = dojo.create("button",{innerHTML:"Next"},div);
         
         div.style.display = "block";
