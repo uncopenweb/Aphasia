268f9dc13b189edf772d177057d10feb2bf70e03
Wai Yau <wyau@email.unc.edu>
2011-02-18 22:31:16 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -208,8 +208,8 @@
         
         var gridDiv = dojo.create("div",{className:"grid",style:{"width":"400px"},innerHTML:"Select the existing interface you want."},div);
         var grid = new dojox.grid.DataGrid({
-            structure: layout,
-            store: store
+            store: store,
+            structure: layout
         },dojo.create("div",{style:{"height":"30px"}},gridDiv));
         grid.startup();
         
