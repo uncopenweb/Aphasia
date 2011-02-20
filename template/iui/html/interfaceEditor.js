8813177a7dc55d19605c61682f75e896e4e6619e
Wai Yau <wyau@email.unc.edu>
2011-02-20 15:10:03 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -1,122 +1,153 @@
-    schema = {
-        title: "Aphasia Application",
-        description: "Editing the Pictures and Words on the interface.",
-        type: "object",
-        properties: {
-            topTabs: {
-                type: "array",
-                minItems: 1,
-                maxItems: 3,
-                items: {
-                    type: "object",
-                    properties: {
-                        name: {
-                            type: "string",
-                            title: "Tab Name",
-                            description: "URL Name of the tab (first character must be #)"
-                        },
-                        nameTag: {
-                            type: "string",
-                            title: "Appeared Tab Name",
-                            description: "The name of the tab actually appearing on the page"
-                        },
-                        image: {
-                            type: "string",
-                            title: "Tab Image Name",
-                            description: "The image that describes the tab name"
-                        },
-                        audioImages: {
-                            type: "array",
-                            minItems: 0,
-                            maxItems: 9,
-                            items: {
-                                type: "object",
-                                properties: {
-                                    pic: {
-                                        type: "string",
-                                        title: "Picture name",
-                                        description: "File name of the picture with extension"
-                                    },
-                                    word: {
-                                        type: "string",
-                                        title: "Word",
-                                        description: "Short description of the picture"                                        
-                                    },
-                                    nameTag: {
-                                        type: "string",
-                                        title: "Unique name for Tab",
-                                        description: "URL Name for the tab that contains the picture (must be unique from all other nameTag within audioImages"
-                                    },
-                                    phrase: {
-                                        type: "string",
-                                        title: "Phrase",
-                                        description: "Long descriptive name of the picture"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            bottomTabs: {
-                type: "array",
-                minItems: 1,
-                maxItems: 3,
-                items: {
-                    type: "object",
-                    properties: {
-                        name: {
-                            type: "string",
-                            title: "Tab Name",
-                            description: "URL Name of the tab (first character must be #)"
-                        },
-                        nameTag: {
-                            type: "string",
-                            title: "Appeared Tab Name",
-                            description: "The name of the tab actually appearing on the page"
-                        },
-                        image: {
-                            type: "string",
-                            title: "Tab Image Name",
-                            description: "The image that describes the tab name"
-                        },
-                        audioImages: {
-                            type: "array",
-                            minItems: 0,
-                            maxItems: 9,
-                            items: {
-                                type: "object",
-                                properties: {
-                                    pic: {
-                                        type: "string",
-                                        title: "Picture name",
-                                        description: "File name of the picture with extension"
-                                    },
-                                    word: {
-                                        type: "string",
-                                        title: "Word",
-                                        description: "Short description of the picture"                                        
-                                    },
-                                    nameTag: {
-                                        type: "string",
-                                        title: "Unique name for Tab",
-                                        description: "URL Name for the tab that contains the picture (must be unique from all other nameTag within audioImages"
-                                    },
-                                    phrase: {
-                                        type: "string",
-                                        title: "Phrase",
-                                        description: "Long descriptive name of the picture"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }  
-    }
+    // schema = {
+        // title: "Aphasia Application",
+        // description: "Editing the Pictures and Words on the interface.",
+        // type: "object",
+        // properties: {
+            // topTabs: {
+                // type: "array",
+                // minItems: 1,
+                // maxItems: 3,
+                // items: {
+                    // type: "object",
+                    // properties: {
+                        // name: {
+                            // type: "string",
+                            // title: "Tab Name",
+                            // description: "URL Name of the tab (first character must be #)"
+                        // },
+                        // nameTag: {
+                            // type: "string",
+                            // title: "Appeared Tab Name",
+                            // description: "The name of the tab actually appearing on the page"
+                        // },
+                        // image: {
+                            // type: "string",
+                            // title: "Tab Image Name",
+                            // description: "The image that describes the tab name"
+                        // },
+                        // audioImages: {
+                            // type: "array",
+                            // minItems: 0,
+                            // maxItems: 9,
+                            // items: {
+                                // type: "object",
+                                // properties: {
+                                    // pic: {
+                                        // type: "string",
+                                        // title: "Picture name",
+                                        // description: "File name of the picture with extension"
+                                    // },
+                                    // word: {
+                                        // type: "string",
+                                        // title: "Word",
+                                        // description: "Short description of the picture"                                        
+                                    // },
+                                    // nameTag: {
+                                        // type: "string",
+                                        // title: "Unique name for Tab",
+                                        // description: "URL Name for the tab that contains the picture (must be unique from all other nameTag within audioImages"
+                                    // },
+                                    // phrase: {
+                                        // type: "string",
+                                        // title: "Phrase",
+                                        // description: "Long descriptive name of the picture"
+                                    // }
+                                // }
+                            // }
+                        // }
+                    // }
+                // }
+            // },
+            // bottomTabs: {
+                // type: "array",
+                // minItems: 1,
+                // maxItems: 3,
+                // items: {
+                    // type: "object",
+                    // properties: {
+                        // name: {
+                            // type: "string",
+                            // title: "Tab Name",
+                            // description: "URL Name of the tab (first character must be #)"
+                        // },
+                        // nameTag: {
+                            // type: "string",
+                            // title: "Appeared Tab Name",
+                            // description: "The name of the tab actually appearing on the page"
+                        // },
+                        // image: {
+                            // type: "string",
+                            // title: "Tab Image Name",
+                            // description: "The image that describes the tab name"
+                        // },
+                        // audioImages: {
+                            // type: "array",
+                            // minItems: 0,
+                            // maxItems: 9,
+                            // items: {
+                                // type: "object",
+                                // properties: {
+                                    // pic: {
+                                        // type: "string",
+                                        // title: "Picture name",
+                                        // description: "File name of the picture with extension"
+                                    // },
+                                    // word: {
+                                        // type: "string",
+                                        // title: "Word",
+                                        // description: "Short description of the picture"                                        
+                                    // },
+                                    // nameTag: {
+                                        // type: "string",
+                                        // title: "Unique name for Tab",
+                                        // description: "URL Name for the tab that contains the picture (must be unique from all other nameTag within audioImages"
+                                    // },
+                                    // phrase: {
+                                        // type: "string",
+                                        // title: "Phrase",
+                                        // description: "Long descriptive name of the picture"
+                                    // }
+                                // }
+                            // }
+                        // }
+                    // }
+                // }
+            // }
+        // }  
+    // }
     
-    thisSchema = {};
+    thisSchema = {
+        "topTabs": [
+           {
+                "name": "",
+                "nameTag":"",
+                "image": "",
+                "audioImages": [
+                    {
+                        "pic":"",
+                        "word":"",
+                        "nameTag":"",
+                        "phrase":""
+                    }
+                ]
+            }
+        ],
+        "bottomTabs":[
+           {
+                "name": "",
+                "nameTag":"",
+                "image": "",
+                "audioImages": [
+                    {
+                        "pic":"",
+                        "word":"",
+                        "nameTag":"",
+                        "phrase":""
+                    }
+                ]
+           }
+        ]
+    };
     
     
     function start() {
