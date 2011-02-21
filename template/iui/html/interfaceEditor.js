1f39421698a3241cac40c4376afab9032ed84d58
Wai Yau <wyau@email.unc.edu>
2011-02-20 21:38:13 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -260,7 +260,7 @@
         var text2 = dojo.create("input",{type:"file"},name1);
         var label3 = dojo.create("label",{innerHTML:"&nbsp; # Images for Tab: "},name1);
         var text3 = dojo.create("select",{id:"form1"},name1);
-            dojo.connect("option",{value:"",selected:"selected"},text3);
+            dojo.connect("option",{value:" ",selected:"selected",innerHTML:" "},text3);
         for (var i=1; i<10; i++) {
             dojo.create("option",{value:i,innerHTML:i},text3);
         }
