4496242a9769c0b99f97a85e855d558785015951
Wai Yau <wyau@email.unc.edu>
2011-02-20 21:06:44 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -256,8 +256,7 @@
         var name1 = dojo.create("div",{className:"third"},tab1);
         var label1 = dojo.create("label",{innerHTML:"Name of Tab:  "},name1);
         var text1 = dojo.create("input",{className:"text"},name1);
-        dojo.create("p",{innerHTML:"   "},name1);
-        var label2 = dojo.create("label",{innerHTML:"Tab Picture: "},name1);
+        var label2 = dojo.create("label",{innerHTML:"&nbsp; Tab Picture: "},name1);
         var text2 = dojo.create("input",{type:"file"},name1);
         var label3 = dojo.create("label",{innerHTML:"# Images for Tab: "},name1);
         var text3 = dojo.create("input",{className:"text"},name1);
