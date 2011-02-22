e52418922bc2f2f63cf04d417e9e9785cbc6aeb5
Wai Yau <wyau@email.unc.edu>
2011-02-21 20:48:30 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -260,7 +260,7 @@
         var text2 = dojo.create("input",{type:"file"},name1);
         var label3 = dojo.create("label",{innerHTML:"&nbsp; # Images for Tab: "},name1);
         var text3 = dojo.create("select",{id:"form1"},name1);
-        for (var i=1; i<10; i++) {
+        for (var i=0; i<10; i++) {
             dojo.create("option",{value:i,innerHTML:i},text3);
         }
             dojo.connect(text3,'onchange',dojo.hitch(this,dynamicForm,text3,tab1,1));
