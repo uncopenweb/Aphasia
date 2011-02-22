201db18e94ba39637e074df1e6b04f9c9cb01046
Wai Yau <wyau@email.unc.edu>
2011-02-21 21:04:53 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -312,9 +312,9 @@
             var td = dojo.create("td",{className:"contentItem"},tr);
             dojo.create("label",{innerHTML:"Word for Pic #"+v+": "},td);
             dojo.create("input",null,td);
-            var td = dojo.create("td",{style:{"border":"1px solid black","width":"45%"}},tr);
+            var td = dojo.create("td",{style:{"border":"1px solid black","width":"40%"}},tr);
             dojo.create("label",{innerHTML:"Phrase: "},td);
-            dojo.create("input",{style:{"width":"95%"}},td);
+            dojo.create("input",{style:{"width":"100%"}},td);
         }
     }
     
