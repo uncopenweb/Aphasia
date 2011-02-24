c99e3bf4b25d8a1b6f147b9eb2d9501dd9585b30
Wai Yau <wyau@email.unc.edu>
2011-02-23 21:46:46 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -178,7 +178,7 @@
                     dojo.byId(ulList[i]).className = "tabs";
                 
                     button.parentNode.style.display="none";
-                    dojo.byId(ulList[++i]).style.display="block";
+                    dojo.byId(ids[++i]).style.display="block";
                     jsProgress.update({progress:i});
                     
                     dojo.byId(ulList[i]).className = "td1";
