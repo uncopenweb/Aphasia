3060722951b936428c7f3f7918a11be87b530abc
Wai Yau <wyau@email.unc.edu>
2011-02-23 21:28:49 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -329,11 +329,11 @@
         
         var topTabs = dojo.create("div",{className:"second"},div);
         var bold = dojo.create("b",{innerHTML:"Top Tabs"},topTabs);
-        var table = dojo.create("table",{className:"dataTable"},topTab);
+        var table = dojo.create("table",{className:"dataTable"},topTabs);
         
         var bottomTabs = dojo.create("div",{className:"second"},div);
         var bold = dojo.create("b",{innerHTML:"Bottom Tabs"},bottomTabs);
-        var table = dojo.create("table",{className:"dataTable"},bottomTab);
+        var table = dojo.create("table",{className:"dataTable"},bottomTabs);
         
         
         backwardButton[3] = dojo.create("button",{innerHTML:"Back"},div);
