f15d91cd6f2ff734964afe6593e3f402860c6827
Wai Yau <wyau@email.unc.edu>
2011-03-03 21:26:57 -0500

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -751,12 +751,12 @@
             dojo.create("label",{innerHTML:"Phrase: "},td);
             dojo.create("input",{style:{"width":"100%"},value:item3, id: "contentItem"+i+j+"3"},td);
             if (j!=0) {
-                document.getElementById("contentItem"+i+j+"1").disabled = "disabled";
-                document.getElementById("contentItem"+i+j+"1").className = "disabled";
-                document.getElementById("contentItem"+i+j+"2").disabled = "disabled";
-                document.getElementById("contentItem"+i+j+"2").className = "disabled";
-                document.getElementById("contentItem"+i+j+"3").disabled = "disabled";
-                document.getElementById("contentItem"+i+j+"3").className = "disabled";
+                dojo.byId("contentItem"+i+j+"1").disabled = "disabled";
+                dojo.byId("contentItem"+i+j+"1").className = "disabled";
+                dojo.byId("contentItem"+i+j+"2").disabled = "disabled";
+                dojo.byId("contentItem"+i+j+"2").className = "disabled";
+                dojo.byId("contentItem"+i+j+"3").disabled = "disabled";
+                dojo.byId("contentItem"+i+j+"3").className = "disabled";
             }
         }
     }
