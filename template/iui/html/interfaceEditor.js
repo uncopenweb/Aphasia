b6c6a8efa8b99df45bd0c89b46ea8279e483e450
Wai Yau <wyau@email.unc.edu>
2011-03-19 17:07:15 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -798,7 +798,7 @@
         var td = dojo.create("td",{className:"dataTd", innerHTML:"Phrase"},tr);
         dojo.forEach(forms[0], function(info) {
             if (info[0].disabled == "disabled") {
-                return;
+                return false;
             }
             var tr = dojo.create("tr",null,table);
             var td = dojo.create("td",{className:"dataTd", innerHTML:i},tr);
