4cfd098e3830d86c21d27b417723f21a38e5c784
Wai Yau <wyau@email.unc.edu>
2011-03-19 16:35:04 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -1031,6 +1031,7 @@
     }
     
     function release2(one, two, three, j, a, form) {
+        console.log(j);
         console.log(form);
         if (one.value.trim()!="" && two.value.trim()!="" && three.value.trim()!="") {
             form[j][0].disabled = "";
