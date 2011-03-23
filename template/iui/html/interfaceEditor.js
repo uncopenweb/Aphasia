5fabb746d94775d43090bee733c0ef49da79e2fe
Wai Yau <wyau@email.unc.edu>
2011-03-23 19:53:06 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -784,7 +784,7 @@
         var h4 = dojo.create("div",{className:"first",innerHTML:"Summary"},div);
         
         var topTabs = dojo.create("div",{style:{"margin":"1%"}, className:"second"},div);
-        if (forms[0][0][0].value.trim() != "" || forms[0][0][0].value != null || forms[1][0][0].value.trim() != "" || forms[1][0][0].value != null || forms[2][0][0].value.trim() != "" || forms[2][0][0].value != null) {
+        if (forms[0][0][0].value != null || forms[0][0][0].value.trim() != "" || forms[1][0][0].value != null || forms[1][0][0].value.trim() != ""|| forms[2][0][0].value != null || forms[2][0][0].value.trim() != "") {
             var bold = dojo.create("b",{innerHTML:"Top Tabs"},topTabs);
         }
         if (dojo.byId("form11").options[dojo.byId("form11").selectedIndex].value>0 && forms[0][0][0].value.trim() != "") {
