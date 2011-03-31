0515a7dc09b3915a2e38e240123ddcd92973b843
Wai Yau <wyau@email.unc.edu>
2011-03-30 20:19:05 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -513,7 +513,11 @@
             collection: 'AphasiaJson',
             mode: 'crud' });
         db.then(function(data) {
-            alert(JSON.stringify(data));
+            data.fetch({
+                onItem: function(item) {
+                    alert(JSON.stringify(item));
+                }
+            });
         });
         var mainArea = dojo.byId("mainArea");
         var ulList = ["Step 1 -- Getting Started", "Step 2 -- Customize Your Top Tabs", "Step 3 -- Customize Your Bottom Tabs", "Step 4 -- Summary", "Step 5 -- You're Done!"];
