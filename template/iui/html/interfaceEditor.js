6043c2aef33cbe4d47eb97019dff8108a285122c
Wai Yau <wyau@email.unc.edu>
2011-04-02 15:36:57 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -596,7 +596,7 @@
         var label = dojo.create("label",{innerHTML:"Theme name for interface: "},td);
         var text = dojo.create("input",{id:"themeName"},td);
    
-       alert(JSON.stringify(example));
+       console.log(JSON.stringify(example));
        var forwardButton = dojo.create("button",{innerHTML:"Start"},div);
             dojo.connect(forwardButton,'onclick',dojo.hitch(this,function() {
                 if (c1.checked) {  
