172b5dbd439a9b58a223c2302dd312717743b782
Wai Yau <wyau@email.unc.edu>
2011-04-09 22:43:46 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -1337,6 +1337,7 @@
             }));
         var forwardButton2 = dojo.create("button",{innerHTML:"Confirm/Finish"},div);
             dojo.connect(forwardButton2,'onclick',dojo.hitch(this,function() {
+                console.log(thisItem);
                 rearrange(ids);
             
                 dojo.byId(ulList[3]).className = "tabs";
