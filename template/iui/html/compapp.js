7948c0155b004e9296b8882b569f037abfb0bed7
Wai Yau <wyau@email.unc.edu>
2011-04-18 19:33:04 -0400

    interface editor

--- template/iui/html/compapp.js
+++ template/iui/html/compapp.js
@@ -22,11 +22,20 @@ dojo.declare('compapp',[], {
                 self.createBorderBar(self,tab,tr);
             });
        
+            var index = 0;
             var leftPhrases = ["Please give me a moment to respond.", "I don't understand.", "Can you repeat that, please?", "I need help."];
+            var leftPics = ["stop.png","question.png","repeat.png","help.png"];
             var leftTable = dojo.create("table",{id:"sideTable",style:{"width":'12.5%',"height":'68%'},selected:"true"},dojo.body());
-                dojo.forEach(leftPhrases, function(aPhrase) {
+                /*dojo.forEach(leftPhrases, function(aPhrase) {
                     var td = dojo.create("table",{id:"sidetd1",style:{"height":"25%","width":"100%"},innerHTML:aPhrase},leftTable);
                         dojo.connect(td, 'onmouseup',dojo.hitch(self,'playSound',aPhrase));
+                });*/
+                dojo.forEach(leftPhrases, function(aPhrase) {
+                    var tr = dojo.create("tr",null,leftTable);
+                    var td = dojo.create("td",{id:"sidetd1",style:{"height":"43px"}},tr);
+                    var div = dojo.create("a",{href:"javascript:;"},td);
+                    var img = dojo.create("img",{src:leftPics[index++],width:"45px",height:"43px"},div);
+                        dojo.connect(td, 'onmouseup', dojo.hitch(self, 'playSound', aPhrase));
                 });
             
             
