905ed6fb74ab4886dbe15590c4d756d3e1c512b7
Wai Yau <wyau@email.unc.edu>
2011-04-18 20:01:53 -0400

    interface editor

--- template/iui/html/compapp.js
+++ template/iui/html/compapp.js
@@ -84,19 +84,19 @@ dojo.declare('compapp',[], {
                                 if (self.currentTab.nameTag!=null) {
                                     document.getElementById(self.currentTab.nameTag).setAttribute("selected","false");
                                     for (var i=0; i<documemnt.getElementById("#tTable").getElementsByTagName("td").length; i++) {
-                                        tr.getElementsByTagName("td")[i].className = "";
+                                        documemnt.getElementById("#tTable").getElementsByTagName("td")[i].className = "";
                                     }                                    
                                     for (var i=0; i<documemnt.getElementById("tTable").getElementsByTagName("td").length; i++) {
-                                        tr.getElementsByTagName("td")[i].className = "";
+                                        documemnt.getElementById("tTable").getElementsByTagName("td")[i].className = "";
                                     }
                                 }
                                 else {
                                     document.getElementById("form").setAttribute("selected","false");
                                     for (var i=0; i<documemnt.getElementById("#tTable").getElementsByTagName("td").length; i++) {
-                                        tr.getElementsByTagName("td")[i].className = "";
+                                        documemnt.getElementById("#tTable").getElementsByTagName("td")[i].className = "";
                                     }                                    
                                     for (var i=0; i<documemnt.getElementById("tTable").getElementsByTagName("td").length; i++) {
-                                        tr.getElementsByTagName("td")[i].className = "";
+                                        documemnt.getElementById("tTable").getElementsByTagName("td")[i].className = "";
                                     }
                                 }
                             }
