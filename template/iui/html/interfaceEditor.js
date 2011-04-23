2685049c50bcfd438baafb39c501147c8b03eefe
Wai Yau <wyau@email.unc.edu>
2011-04-23 15:45:49 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -1594,94 +1594,10 @@
                 }
                 data.save();
                 if (item!="") { 
-                    //uploadPictures(ids, item._id);
-                            var def = uow.getDatabase({
-            database: 'Media',
-            collection: 'Image',
-            mode: 'crud' });
-
-        for (var i=2; i<7; i++) {
-            var form1 = dojo.byId("uploadForm"+i);
-            console.log(form1.file.value);
-            if (form1.file.value!="") {
-                def.addCallback(function(db) {
-                    db.upload({
-                        form: form1,
-                        load: function(data, ioArgs) {
-                            console.log('load', data);
-                        },
-                        error: function(msg, ioArgs) {
-                            console.log('error', msg);
-                        }
-                    });
-                });
-            }
-            for (var j=0; j<9; j++) {
-                var form2 = dojo.byId("tab"+i+j);
-                if (form2==null) {
-                    break;
-                }
-                if (form2.file.value!="") {
-                    def.addCallback(function(db) {
-                        db.upload({
-                            form: form2,
-                            load: function(data, ioArgs) {
-                                console.log('load', data);
-                            },
-                            error: function(msg, ioArgs) {
-                                console.log('error', msg);
-                            }
-                        });
-                    });
-                }
-            }       
-        }
-        donePage(ids);
+                    uploadPictures(ids, item._id);
                 }
                 else {
-                    //uploadPictures(ids, thisItem._id);
-                            var def = uow.getDatabase({
-            database: 'Media',
-            collection: 'Image',
-            mode: 'crud' });
-
-        for (var i=2; i<7; i++) {
-            var form1 = dojo.byId("uploadForm"+i);
-            console.log(form1.file.value);
-            if (form1.file.value!="") {
-                def.addCallback(function(db) {
-                    db.upload({
-                        form: form1,
-                        load: function(data, ioArgs) {
-                            console.log('load', data);
-                        },
-                        error: function(msg, ioArgs) {
-                            console.log('error', msg);
-                        }
-                    });
-                });
-            }
-            for (var j=0; j<9; j++) {
-                var form2 = dojo.byId("tab"+i+j);
-                if (form2==null) {
-                    break;
-                }
-                if (form2.file.value!="") {
-                    def.addCallback(function(db) {
-                        db.upload({
-                            form: form2,
-                            load: function(data, ioArgs) {
-                                console.log('load', data);
-                            },
-                            error: function(msg, ioArgs) {
-                                console.log('error', msg);
-                            }
-                        });
-                    });
-                }
-            }       
-        }
-        donePage(ids);
+                    uploadPictures(ids, thisItem._id);
                 }
             });
             db.addErrback(function(msg) {
@@ -1696,13 +1612,13 @@
             collection: 'Image',
             mode: 'crud' });
 
-        for (var i=1; i<7; i++) {
-            var form1 = dojo.byId("uploadForm"+i);
+        for (var i=1; i<2; i++) {
+            var form1 = dojo.byId("uploadForm"+1);
             console.log(form1.file.value);
             if (form1.file.value!="") {
                 def.addCallback(function(db) {
                     db.upload({
-                        form: form1,
+                        form: dojo.byId("uploadForm"+1),
                         load: function(data, ioArgs) {
                             console.log('load', data);
                         },
