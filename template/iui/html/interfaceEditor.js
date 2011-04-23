62aa3b2e57dd5960061a586ceaf1fcc20afef43e
Wai Yau <wyau@email.unc.edu>
2011-04-23 15:27:00 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -1093,6 +1093,7 @@
                 item5 = "";
             }
             var v = j+1;
+            console.log(dojo.byId("tab"+i));
             var div = dojo.create("form",{method:"POST", enctype:"multipart/form-data",id:"tab"+i+j},dojo.byId("tab"+i));
             var table = dojo.create("table",{className:"contentTable"},div);
             var tr = dojo.create("tr",{className:"contentRow"},table);
