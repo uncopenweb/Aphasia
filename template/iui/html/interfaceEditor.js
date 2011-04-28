1f8ec472ce3c580edb0c452855d88187246c6438
Wai Yau <wyau@email.unc.edu>
2011-04-27 20:42:00 -0400

    interface editor

--- template/iui/html/interfaceEditor.js
+++ template/iui/html/interfaceEditor.js
@@ -937,20 +937,20 @@
         var label2 = dojo.create("label",{innerHTML:"&nbsp; Tab Picture: "},tab1);
         if (uploadForm[j]==false) {
             var text2 = dojo.create("input",{id:"text12"+k,type:"file", name:"file", disabled:"disabled",className:"disabled"},tab1);
+            if (text1.value.trim()!="") {
+                text2.disabled = "";
+                text2.className = "";
+            }
+            var a = dojo.create("input",{type:"button",value:"Clear", disabled:"disabled"},tab1);
+            var label3 = dojo.create("label",{innerHTML:"&nbsp; # Images for Tab: "},tab1);
+            var text3 = dojo.create("select",{id:"form1"+k, disabled:"disabled",className:"disabled"},tab1);
+            if (text2.value.trim()!="") {
+                text3.disabled = "";
+                text3.className = "";
+            }
         }
         else {
             var text2 = dojo.create("a",{href:uploadForm[j],innerHTML:uploadForm[j]},tab1);
-        }
-        if (text1.value.trim()!="") {
-            text2.disabled = "";
-            text2.className = "";
-        }
-        var a = dojo.create("input",{type:"button",value:"Clear", disabled:"disabled"},tab1);
-        var label3 = dojo.create("label",{innerHTML:"&nbsp; # Images for Tab: "},tab1);
-        var text3 = dojo.create("select",{id:"form1"+k, disabled:"disabled",className:"disabled"},tab1);
-        if (text2.value.trim()!="") {
-            text3.disabled = "";
-            text3.className = "";
         }
         
         var name = dojo.create("input",{name:"description",value:"uploadForm"+k,style:{"display":"none"}},tab1);
